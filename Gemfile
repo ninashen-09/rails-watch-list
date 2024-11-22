b888bdd35d4ac48f9bf3bb5e9761c105ed0951f1
Nini S <ninashen1998@gmail.com>
2024-11-22 17:10:45 +0800

    checking process

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem "sassc-rails" # Uncomment this line
 
 gem "dotenv-rails"
 
+gem "cloudinary"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.1.5"
 
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,19 @@
 class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
   def new
     @bookmark = Bookmark.new
     @list = List.find(params[:list_id])
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to lists_path
+      redirect_to list_bookmarks_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -16,7 +22,7 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
 end
